c7625517adafa6cb580efc4992977ae68b609449
Stephyarr <stephaniearratia21@gmail.com>
2023-03-08 23:54:46 -0300

    tabNavigation

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,8 @@
 export default {
-  primary: "#F991CC",
-  secondary: "#E2AFDE",
-  tertiary: "#D3D2C7",
-  background: "#D3C2CE",
+  primary: "#3E885B",
+  secondary: "#85bda6",
+  tertiary: "#BEDCFE",
+  background: "#C0D7BB",
   text: "#212121",
   white: "#FFFFFF",
   black: "#000000",
